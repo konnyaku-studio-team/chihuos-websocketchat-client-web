7e84f2ec89d40fe70cf48e4081737ee09c029f9c
chihuo2104 <chihuoqwefengmi13@qq.com>
2020-07-27 18:47:47 +0800

    Fixed some bugs....
    The bug is very very difficult to fix!

--- message.js
+++ message.js
@@ -5,6 +5,10 @@ function connectws(){
     wss.onmessage=function(msg){
         messagecontrol.innerHTML+="<br>"+msg.data;
         if("Notification" in Window){
+            alert("草，你的浏览器是不是IE的？怎么连这个也不支持？");
+        }
+        else
+        {
             if(Notification.permission === "granted"){
                 msNotify = new Notification("新消息！",{body:msg.data});
             }
@@ -15,12 +19,12 @@ function connectws(){
                         if(Notification.permission === "granted"){
                             msNotify = new Notification("新消息！",{body:msg.data});
                         }
-                    })
+                    });
                 }
             }
         }
-        console.log(msg.data);
-    }
+    console.log(msg.data);
+    }    
 }
 function commitws(){
    var messageinput=document.getElementById("messageinput");
