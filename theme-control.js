df7e01f2150ead6d5c18e134570da47613941bd6
chihuo2104 <chihuoqwefengmi13@qq.com>
2020-08-21 07:27:07 -0400

    Added HTML link.
    Added HTML link.
    index.html to theme-control.css

--- theme-control.js
+++ theme-control.js
@@ -1,14 +1,16 @@
-function hiddenwindow(wnm){
-    console.debug("[debug]["+new Date()+"]运行了hiddenwindow函数！");
-    var connectwindow=document.getElementById(wnm);
-    var mask=document.getElementById("mask");
-    connectwindow.style.display="none";
-    mask.style.display="none";
-}
+// 显示窗口。
 function openwindow(wnm){
     console.debug("[debug]["+new Date()+"]运行了openwindow函数！");
     var connectwindow=document.getElementById(wnm);
     var mask=document.getElementById("mask");
     connectwindow.style.display="block";
     mask.style.display="block";
+}
+// 隐藏窗口。
+function hiddenwindow(wnm){
+    console.debug("[debug]["+new Date()+"]运行了hiddenwindow函数！");
+    var connectwindow=document.getElementById(wnm);
+    var mask=document.getElementById("mask");
+    connectwindow.style.display="none";
+    mask.style.display="none";
 }
