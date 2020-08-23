584e1ce4b7607c0753447f39c63753ab7fdc3c98
chihuo2104 <chihuoqwefengmi13@qq.com>
2020-08-23 07:01:40 -0400

    Added emoji parse.
    The emoji parser has added!

--- system.js
+++ system.js
@@ -43,5 +43,69 @@ function changeSettingData(){
 function addEmote(emotename){
     console.debug("[debug]["+new Date()+"]运行了addEmote函数！");
     var messageinput=document.getElementById("messageinput");
-    messageinput.value+="{$emote:"+emotename+":etmoe$}";
+    messageinput.value+="{$emote:"+emotename+":etome$}";
+}
+var base64=new Base64();
+const EMOTE_FILE_CDN="https://cdn.jsdelivr.net/gh/chihuos-wschat-filecdn/emote@master";//目前使用GayHub本人CDN源，以后设置可以调整吧......
+function parsemotedata(realmsg,ename){
+    while(realmsg.indexOf("{$emote:"+emotedata+":etome$}")!=-1){
+        realmsg=realmsg.replace("{$emote:"+emotedata+":etome$}","<img src=\""+EMOTE_FILE_CDN+"/"+ename+".png\" class=\"emote-show\">");
+    }
+    return realmsg;
+}
+// EMOTE_FILE_CDN以后可以变化
+function connectws(){   
+    console.debug("[debug]["+new Date()+"]运行了connectws函数！");
+    var serversite=document.getElementById("serversite");
+    var messagecontrol=document.getElementById("messagecontrol");
+    if(storage_setting){
+        localStorage.setItem("server_address",serversite.value);
+    }
+    try{
+        wss=new WebSocket(serversite.value);
+    }catch(wse){
+        alert("服务器连接错误！错误原因："+wse);
+    }
+    wss.onopen=function(){
+        alert("连接成功！");
+        wss.onmessage=function(msg){
+            console.debug("[debug]["+new Date()+"]接收到了ws内容！内容："+msg.data);
+            var realmsg=base64.decode(msg.data);
+            realmsg=parsemotedata(realmsg,"tv_doge");
+            realmsg=parsemotedata(realmsg,"tv_ll");
+            realmsg=parsemotedata(realmsg,"tv_yx");
+            realmsg=parsemotedata(realmsg,"tv_angry");
+            realmsg=parsemotedata(realmsg,"tv_cute");
+            realmsg=parsemotedata(realmsg,"tv_dk");
+            realmsg=parsemotedata(realmsg,"tv_dz");
+            realmsg=parsemotedata(realmsg,"tv_hx");
+            var parsedmsg=realmsg;
+            messagecontrol.innerHTML+="<br>"+parsedmsg;
+            // String类QAQ
+            if("Notification" in Window){
+                alert("草，你的浏览器是不是IE的？怎么连这个也不支持？");
+            }
+            else
+            {
+                if(Notification.permission === "granted"){
+                    msNotify = new Notification("新消息！",{body:base64.decode(msg.data)});
+                }
+                else
+                {
+                    if(Notification.permission === "default"){
+                        Notification.requestPermission(function(){
+                            if(Notification.permission === "granted"){
+                                msNotify = new Notification("新消息！",{body:base64.decode(msg.data)});
+                            }
+                        });
+                    }
+                }
+            };
+    };
+    var connectwindow=document.getElementById("connectwindow");
+    var mask=document.getElementById("mask");
+    connectwindow.style.display="none";
+    mask.style.display="none";
+    console.log(msg.data);
+    }    
 }
